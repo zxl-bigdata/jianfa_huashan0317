ab2eeaddcdecf610f34f6045230c3b087e6a46ee
zhangxilin <zxl_bigdata@126.com>
2020-08-18 21:48:16 +0800

    加了个令狐冲

--- src/main/java/com/atguigu/jianfa/Dugu.java
+++ src/main/java/com/atguigu/jianfa/Dugu.java
@@ -2,6 +2,27 @@ package com.atguigu.jianfa;
 
 public class Dugu {
     public static void main(String[] args) {
+        System.out.println("紫霞神功夫好");
+        System.out.println("紫霞神功夫好");
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
         System.out.println("独孤剑法好");
         System.out.println("独孤剑法好");
         System.out.println("独孤剑法好");
