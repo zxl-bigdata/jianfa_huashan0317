30844690c1617d978b0e280acc6a47a7ffe8c7c9
zhangxilin <zxl_bigdata@126.com>
2020-08-18 21:06:27 +0800

    Initial commit

---
+++ src/main/java/com/atguigu/jianfa/Dugu.java
@@ -0,0 +1,10 @@
+package com.atguigu.jianfa;
+
+public class Dugu {
+    public static void main(String[] args) {
+        System.out.println("独孤剑法好");
+        System.out.println("独孤剑法好");
+        System.out.println("独孤剑法好");
+        System.out.println("独孤剑法好");
+    }
+}
