84eb245eff98ad572b508b8049b02e0ce4ddcc47
zhangxilin <zxl_bigdata@126.com>
2020-08-18 21:48:23 +0800

    加了个令狐冲

--- src/main/java/com/atguigu/jianfa/NewDugu.java
+++ src/main/java/com/atguigu/jianfa/NewDugu.java
@@ -2,6 +2,23 @@ package com.atguigu.jianfa;
 
 public class NewDugu {
     public static void main(String[] args) {
+        System.out.println("独孤----令狐冲----剑法好");
+        System.out.println("独孤----令狐冲----剑法好");
+        System.out.println("独孤----令狐冲----剑法好");
+        System.out.println("独孤----令狐冲----剑法好");
+        System.out.println("独孤----令狐冲----剑法好");
+        System.out.println("独孤----令狐冲----剑法好");
+        System.out.println("独孤----令狐冲----剑法好");
+        System.out.println("独孤----令狐冲----剑法好");
+        System.out.println("独孤----令狐冲----剑法好");
+        System.out.println("独孤----令狐冲----剑法好");
+        System.out.println("独孤----令狐冲----剑法好");
+        System.out.println("独孤----令狐冲----剑法好");
+
+
+
+
+
         System.out.println("独孤剑法好");
         System.out.println("独孤剑法好");
         System.out.println("独孤剑法好");
